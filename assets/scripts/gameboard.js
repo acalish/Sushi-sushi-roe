e4edf1fa6519422e867fe2a5267d7e2f17cc5222
acalish <acalish@gmail.com>
2018-08-09 09:59:43 -0400

    Update game logic for winning scenarios
    - add giant if else statement that returns true if there is a winner

--- assets/scripts/gameboard.js
+++ assets/scripts/gameboard.js
@@ -20,11 +20,41 @@ $(() => {
 
 const gameBoard = ['', '', '', '', '', '', '', '', '']
 
-// const winningCombinations = (currentBoard[0] === currentBoard[1] === currentBoard[2])
-// || (currentBoard[0] === currentBoard[1] === currentBoard[2])
-// || (currentBoard[3] === currentBoard[4] === currentBoard[5])
-// || (currentBoard[6] === currentBoard[7] === currentBoard[8])
-// || (currentBoard[0] === currentBoard[3] === currentBoard[6])
-// || (currentBoard[1] === currentBoard[4] === currentBoard[7])
-// || (currentBoard[0] === currentBoard[4] === currentBoard[8])
-// || (currentBoard[2] === currentBoard[4] === currentBoard[6])
+const checkWinner = function () {
+  if (gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2] && gameBoard[0] !== '') {
+    return true
+  } else if (gameBoard[1] === gameBoard[2] && gameBoard[1] === gameBoard[3] && gameBoard[1] !== '') {
+    return true
+  } else if (gameBoard[3] === gameBoard[4] && gameBoard[3] === gameBoard[5] && gameBoard[3] !== '') {
+    return true
+  } else if (gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8] && gameBoard[6] !== '') {
+    return true
+  } else if (gameBoard[0] === gameBoard[3] && gameBoard[0] === gameBoard[6] && gameBoard[0] !== '') {
+    return true
+  } else if (gameBoard[1] === gameBoard[4] && gameBoard[1] === gameBoard[7] && gameBoard[1] !== '') {
+    return true
+  } else if (gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8] && gameBoard[0] !== '') {
+    return true
+  } else if (gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6] && gameBoard[2] !== '') {
+    return true
+  } else {
+    return false
+  }
+}
+
+
+if (
+  (gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2] && gameBoard[0] !== '') ||
+  (gameBoard[1] === gameBoard[2] && gameBoard[1] === gameBoard[3] && gameBoard[1] !== '') ||
+  (gameBoard[3] === gameBoard[4] && gameBoard[3] === gameBoard[5] && gameBoard[3] !== '') ||
+  (gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8] && gameBoard[6] !== '') ||
+  (gameBoard[0] === gameBoard[3] && gameBoard[0] === gameBoard[6] && gameBoard[0] !== '') ||
+  (gameBoard[1] === gameBoard[4] && gameBoard[1] === gameBoard[7] && gameBoard[1] !== '') ||
+  (gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8] && gameBoard[0] !== '') ||
+  (gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6] && gameBoard[2] !== '')
+) {
+  return true
+} else {
+  return false
+}
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -10,6 +10,28 @@ $(() => {
   // your JS code goes here
   const gameBoard = ['', '', '', '', '', '', '', '', '']
   let currentPlayer = 'X'
+  const checkWinner = function () {
+    if (gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2] && gameBoard[0] !== '') {
+      return true
+    } else if (gameBoard[1] === gameBoard[2] && gameBoard[1] === gameBoard[3] && gameBoard[1] !== '') {
+      return true
+    } else if (gameBoard[3] === gameBoard[4] && gameBoard[3] === gameBoard[5] && gameBoard[3] !== '') {
+      return true
+    } else if (gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8] && gameBoard[6] !== '') {
+      return true
+    } else if (gameBoard[0] === gameBoard[3] && gameBoard[0] === gameBoard[6] && gameBoard[0] !== '') {
+      return true
+    } else if (gameBoard[1] === gameBoard[4] && gameBoard[1] === gameBoard[7] && gameBoard[1] !== '') {
+      return true
+    } else if (gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8] && gameBoard[0] !== '') {
+      return true
+    } else if (gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6] && gameBoard[2] !== '') {
+      return true
+    } else {
+      return false
+    }
+  }
+  //
   $('.box').on('click', function (event) {
     console.log('you clicked', event.target.id)
     // replaces array index with X
@@ -23,5 +45,7 @@ $(() => {
       $(event.target).text('O')
       currentPlayer = 'X'
     }
+    checkWinner()
+    console.log(checkWinner())
   })
 })
